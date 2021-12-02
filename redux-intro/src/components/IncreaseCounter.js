c09dbe19eb0fbd0f659697b6afc4c90cdd931e9d
yusuf Demir <yusuf.ferrum@gmail.com>
2021-12-02 19:56:48 +0100

    388de kaldim

--- redux-intro/src/components/IncreaseCounter.js
+++ redux-intro/src/components/IncreaseCounter.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import {increaseCounter} from '../redux/actions/counterActions'
 
-export default class IncreaseCounter extends Component {
+ class IncreaseCounter extends Component {
     render() {
         return (
             <div>
-                
+                <button onClick={(e)=>{
+                    this.props.dispatch(increaseCounter())
+                }}>1 arttir</button>
             </div>
         )
     }
 }
+
+
+function mapDispatchToProps(dispatch){
+    return {actions:bindActionCreators(increaseCounter,dispatch)}
+}
+
+export default connect(mapDispatchToProps)(IncreaseCounter)
